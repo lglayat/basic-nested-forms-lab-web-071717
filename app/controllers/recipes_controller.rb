3564f44cefc7bede483902c4b6eb6f82a6246768
lglayat <glayatle@g.cofc.edu>
2017-08-16 12:06:23 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,28 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def create
+    # byebug
+    @recipe = Recipe.create(recipe_params)
+    # @recipe.ingredients.build
+    # @recipe.ingredients.build
+
+    # @recipe.save
+
+    redirect_to recipe_path(@recipe)
+
+  end
+
+
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title,
+    ingredients_attributes: [:name, :quantity])
   end
 end
